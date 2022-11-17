449ca15bb9e44540aa1d3e0d3d28c3e5fd7daeac
Prerit Vishal <70009672+preritvishal@users.noreply.github.com>
2022-11-17 08:34:02 +0530

    Created Calculator.java file

---
+++ prerit/Calculator.java
@@ -0,0 +1,14 @@
+class Calculator {
+	
+	void add(int a, int b)	{
+		System.out.println(a + b);
+	}
+	
+	void sub(int a, int b) {
+		System.out.println(a - b);
+	}
+	
+	public static void main(String[] args) {
+			
+	}
+}
