fcfca8cd43bfcea91a5873680c23069b44010013
Prerit Vishal <preritvishal3@gmail.com>
2022-11-17 08:49:31 +0530

    Calculator : completed all 4 baisc functions

--- prerit/Calculator.java
+++ prerit/Calculator.java
@@ -8,7 +8,23 @@ class Calculator {
 		System.out.println(a - b);
 	}
 	
+	void mul(int a, int b)	{
+		System.out.println(a * b);
+	} 
+
+	void div(int a, int b) {
+		System.out.println(a / b);
+	}
+
 	public static void main(String[] args) {
-			
+		Calculator obj = new Calculator();
+
+		obj.add(10, 20);
+
+		obj.sub(10, 20);
+
+		obj.mul(10, 20);
+		
+		obj.div(10, 20);
 	}
 }
