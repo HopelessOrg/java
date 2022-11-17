f285d9a3f0e3785e45e466805a55d05c5a534fb0
Prerit Vishal <preritvishal3@gmail.com>
2022-11-17 09:12:13 +0530

    Calculator: updated with comments, toString() and return types

--- prerit/Calculator.java
+++ prerit/Calculator.java
@@ -1,30 +1,56 @@
 class Calculator {
 	
-	void add(int a, int b)	{
-		System.out.println(a + b);
+	// These are void methods, they don't return anything
+	
+	// void add(int a, int b)	{
+	// 	System.out.println(a + b);
+	// }
+	
+	// void sub(int a, int b) {
+	// 	System.out.println(a - b);
+	// }
+	
+	// void mul(int a, int b)	{
+	// 	System.out.println(a * b);
+	// } 
+
+	// void div(int a, int b) {
+	// 	System.out.println(a / b);
+	// }
+
+	int add(int a, int b)	{
+		return (a + b);
 	}
 	
-	void sub(int a, int b) {
-		System.out.println(a - b);
+	int sub(int a, int b) {
+		return (a - b);
 	}
 	
-	void mul(int a, int b)	{
-		System.out.println(a * b);
+	int mul(int a, int b)	{
+		return (a * b);
 	} 
 
-	void div(int a, int b) {
-		System.out.println(a / b);
+	int div(int a, int b) {
+		return (a / b);
 	}
-
+	
+	// overriding the toString() will allow us to customise the output when we'll print the object
+	public String toString() {
+		return "this is an instance of Calculator class";
+	}
 	public static void main(String[] args) {
 		Calculator obj = new Calculator();
 
-		obj.add(10, 20);
-
-		obj.sub(10, 20);
-
-		obj.mul(10, 20);
+		int output = obj.add(10, 20);
+		System.out.println(output);
 		
-		obj.div(10, 20);
+		output = obj.sub(10, 20);
+		System.out.println(output);
+		
+		output = obj.mul(10, 20);
+		System.out.println(output);
+		
+		output = obj.div(10, 20);
+		System.out.println(output);
 	}
 }
